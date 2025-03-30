3f2bb7a3717334a09c1681fef30d2fe1f9b4c0ec
iamseung <seesungseok@naver.com>
2025-03-30 22:57:24 +0900

    ✨전역 예외 처리 초안

---
+++ src/main/java/com/example/recommendationservice/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.recommendationservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    NOT_EXIST_CATEGORY("존재하지 않는 카테고리 입니다.", HttpStatus.NOT_FOUND),
+    NOT_EXIST_BRAND("존재하지 않는 브랜드 입니다.", HttpStatus.NOT_FOUND),
+    DUPLICATE_BRAND_NAME("이미 존재하는 브랜드 이름입니다.", HttpStatus.CONFLICT),
+    NOT_EXIST_PRODUCT("존재하지 않는 상풉입니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final HttpStatus status;
+}
